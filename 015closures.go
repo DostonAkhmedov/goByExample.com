8a42ade4b6442cf9b0e3d3e24e8733089d1ea7da
Doston <akhmedovdoston@gmail.com>
2017-12-21 11:44:03 +0500

    add new examples

---
+++ 015closures.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func intSeq() func() int {
+	var i int = 0
+	return func() int {
+		i ++;
+		return i
+	}
+}
+
+func main() {
+
+	nextInt := intSeq()
+
+
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+
+	var newInts = intSeq()
+	fmt.Println(newInts())
+
+}
