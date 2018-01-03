9a6316de6595ce53db69dd079752fb57faac8e43
Doston <akhmedovdoston@gmail.com>
2018-01-04 02:55:19 +0500

    add new examples

---
+++ 039sorting.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"sort"
+	"fmt"
+)
+
+func main() {
+
+	strs := []string{"c", "a", "b"}
+	sort.Strings(strs)
+	fmt.Println("Strings:", strs)
+
+	ints := []int{2, 1, 5}
+	sort.Ints(ints)
+	fmt.Println("Ints:", ints)
+
+	s := sort.IntsAreSorted(ints)
+	fmt.Println("Sorted:", s)
+
+}
